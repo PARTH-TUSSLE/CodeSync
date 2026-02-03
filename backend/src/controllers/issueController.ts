4c9ef94a1a2e208cee610db28b3e5fdf176f68c2
PARTH-TUSSLE <parthgartan26feb@gmail.com>
2026-02-03 18:01:01 +0530

    added issue creation logic

--- backend/src/controllers/issueController.ts
+++ backend/src/controllers/issueController.ts
@@ -1,7 +1,46 @@
 import type { Request, Response } from "express";
+import { prisma } from "../prisma.js";
 
-export const createIssue = (req: Request, res: Response) => {
-  res.send("Issue created");
+export const createIssue = async (req: Request, res: Response) => {
+  
+  const { title, description, status, repoID, creatorID } = req.body;
+
+  if ( !title || !status || !repoID || !creatorID ) {
+    return res.status(400).json({
+      msg: `Fields title, status, repoID, creatorID are required !`
+    })
+  }
+
+  try {
+
+    const createdIssue = await prisma.issue.create({
+      data: {
+        title: title,
+        description: description,
+        status: status,
+        repositoryId: repoID,
+        authorId: creatorID
+      }
+    })
+
+    if (createdIssue) {
+      return res.status(201).json({
+        msg: `Issue created successfully !`,
+        createdIssue
+      })
+    }
+
+    return res.status(400).json({
+      msg: `Some error occurred while creating the issue`
+    })
+    
+  } catch (error) {
+    const errMsg = error instanceof Error ? error.message : String(error);
+    return res.status(500).json({
+      msg: `Error occurred while creating the issue ${errMsg}`
+    })
+  }
+
 };
 
 export const updateIssueByID = (req: Request, res: Response) => {
