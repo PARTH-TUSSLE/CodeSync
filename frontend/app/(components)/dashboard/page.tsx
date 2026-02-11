09b5963bd7c78e77f81ff10f7c9dd44fcfac745c
PARTH-TUSSLE <parthgartan26feb@gmail.com>
2026-02-11 18:48:08 +0530

    fixed tailwind config errors

--- frontend/app/(components)/dashboard/page.tsx
+++ frontend/app/(components)/dashboard/page.tsx
@@ -4,11 +4,48 @@ import React, { useEffect, useState } from "react";
 import axios from "axios";
 
 function page() {
-  const [repositories, setRepositories] = useState([]);
-  const [searchQuery, setSearchQuery] = useState([]);
-  const [suggestedRepositories, setSuggestedRepositories] = useState([]);
-  const [searchResults, setSearchResults] = useState([]);
+  
+  interface Repository {
+    id: string;
+    name: string;
+    description?: string;
+    content: string[];
+    visibility: boolean;
+    ownerId: string;
+    createdAt: Date;
+    updatedAt: Date;
+  }
 
+  const [repositories, setRepositories] = useState<Repository[]>([]);
+  const [searchQuery, setSearchQuery] = useState<string>("");
+  const [suggestedRepositories, setSuggestedRepositories] = useState<
+    Repository[]
+  >([]);
+  const [searchResults, setSearchResults] = useState<Repository[]>([]);
+
+
+      useEffect(() => {
+        const userId = localStorage.getItem("userId");
+        const token = localStorage.getItem("token");
+
+        console.log("userId", userId);
+        console.log("token", token);
+
+        const fetchUserRepos = async () => {
+          const response = await axios.get(
+            `http://localhost:8000/repo/user/${userId}`,
+            {
+              headers: {
+                Authorization: `Bearer ${token}`,
+              },
+            },
+          );
+          const userRepos = await response.data.userRepos;
+          console.log(userRepos);
+          setRepositories(userRepos);
+        };
+        fetchUserRepos();
+      }, []);
 
   useEffect(() => {
     const fetchUserRepos = async () => {
@@ -21,8 +58,15 @@ function page() {
   }, []);
 
   useEffect(() => {
-    
-  }, [])
+    if (searchQuery === "") {
+      setSearchResults(repositories);
+    }
+    const filteredRepos = repositories.filter((repo) => {
+      repo.name.toLowerCase().includes(searchQuery.toLowerCase());
+    });
+    setSearchResults(filteredRepos);
+  }, []);
+
 
   return <div></div>;
 }
